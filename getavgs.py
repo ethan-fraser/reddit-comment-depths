2d668bb69d55a020ab755208c07352b6d09a8fb1
Ethan Fraser <ejpfraser@gmail.com>
2020-11-23 14:33:29 +1300

    initial commit

---
+++ getavgs.py
@@ -0,0 +1,91 @@
+import praw
+import os
+import sys
+
+# set up reddit instance
+reddit = praw.Reddit(
+    client_id="_2x0vWbs6BnMcQ",
+    client_secret="w96UEy3foaRcNIVQn56OaDMMmTurxA",
+    user_agent="reddit comment depth analysis"
+)
+
+def get_subreddit_from_id(submission_id):
+    return reddit.submission(id=submission_id).subreddit.display_name
+
+
+def breadth_first_search(submission):
+    comments = [[]]
+    comment_queue = submission.comments[:]
+    level_sizes = [len(comment_queue)]
+    i = 0
+    while comment_queue:
+        if level_sizes[i] == 0:
+            i += 1
+            comments.append([])
+        level_sizes[i] -= 1
+        comment = comment_queue.pop(0)
+        comments[i].append(comment)
+        comment_queue.extend(comment.replies)
+        # need to make a new level if we encounter one
+        try:
+            level_sizes[i+1] += len(comment.replies)
+        except IndexError:
+            level_sizes.append(len(comment.replies))
+    return comments
+
+
+def dfs_aux(comment):
+    result = []
+    for reply in comment.replies:
+        result.extend(dfs_aux(reply))
+    return result
+
+def depth_first_search(submission):
+    comments = []
+    for i, comment in enumerate(submission.comments):
+        comments.append([comment])
+        comments[i].extend(dfs_aux(comment))
+    return comments
+
+
+def calculate_averages(comments):
+    avg_scores = []
+    for i in range(len(comments)):
+        total = sum([comment.score for comment in comments[i]])
+        avg_scores.append(total//len(comments[i]))
+    return avg_scores
+
+
+if __name__ == "__main__":
+    subreddit_name = input("Enter the name of the target subreddit: ").strip()
+    output_file = input("Enter the name of the output file: ").strip()
+    if not os.path.exists(output_file):
+        print(f"Error: {output_file}: no such file exists")
+        sys.exit(1)
+    limit = input("Enter the number of posts you want to look at (default = 10): ").strip()
+    if limit == "":
+        print("Setting limit to defualt (10)")
+        limit = 10
+    else:
+        limit = int(limit)
+
+    subreddit = reddit.subreddit(subreddit_name)
+    for submission in subreddit.top("all", limit=limit):
+        print(f"{submission.title} ({submission.id})")
+
+        # fully extend the comment tree for that post
+        # have to do this otherwise not all the comments will be visible
+        submission.comments.replace_more(limit=None)
+
+        # perform a breadth-first-search on the comment tree
+        bf_comments = breadth_first_search(submission)
+        df_comments = depth_first_search(submission)
+
+        # get an average score for each level
+        bf_averages = calculate_averages(bf_comments)
+        df_averages = calculate_averages(df_comments)
+
+        print(f"bf: {bf_averages}\ndf: {df_averages}")
+        with open(output_file, "a") as avgs_file: # <<<< change output file here <<<<<<
+            avgs_file.write(f"{submission.id}:{bf_averages}:{df_averages}\n")
+
---
+++ makeplot.py
@@ -0,0 +1,72 @@
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot as plt
+import numpy as np
+import sys
+from progress.bar import IncrementalBar
+from getavgs import get_subreddit_from_id
+
+# load averages
+def load_averages(filename):
+    ids = []
+    bf_averages = []
+    df_averages = []
+    with open(filename, "r") as avgs_file:
+        lines = avgs_file.readlines()
+    for line in lines:
+        _id, bf_list_str, df_list_str = line.split(":")
+        ids.append(_id)
+        bf_averages.append([int(x) for x in df_list_str.strip('][\n').split(', ')])
+        df_averages.append([int(x) for x in bf_list_str.strip('][\n').split(', ')])
+    return ids, df_averages, bf_averages
+
+def overall_averages(arr):
+    # takes in a 2d array and creates a 1d array
+    # with a length equal to the max subarray of arr,
+    # consisting of the averages for each column
+    result = []
+    max_subarray_size = max([len(x) for x in arr])
+    for j in range(max_subarray_size):
+        for i in range(len(arr)):
+            try:
+                result[j] += arr[i][j]
+            except IndexError:
+                try:
+                    result[j] += 0
+                except IndexError:
+                    result.append(0)
+    for j in range(len(result)):
+        result[j] = result[j]//len(arr)
+    return result
+
+# create plot
+def plot_values(title, ids, averages, x, y, n):
+    plt.subplot(x, y, n)
+    for i in range(len(averages)):
+        x = np.arange(0, len(averages[i]), 1)
+        plt.plot(x, averages[i], ".:", label=ids[i], alpha=0.3, markersize=5)
+    y = overall_averages(averages)
+    x = np.arange(1, len(y)+1, 1)
+    plt.plot(x, y, "b-", label=ids[i])
+    #plt.ylim([0,150])
+    #plt.xlim([1,12])
+    plt.xlabel("Comment level")
+    plt.ylabel("Average score")
+    plt.title(title)
+    #plt.legend()
+
+if __name__ == "__main__":
+    ids = []
+    bf_averages = []
+    df_averages = []
+    for input_file in sys.argv[1:]:
+        _ids, _bf_averages, _df_averages = load_averages(input_file.rstrip())
+        ids.append(_ids)
+        bf_averages.append(_bf_averages)
+        df_averages.append(_df_averages)
+
+    bar = IncrementalBar('Progress', max=len(ids))
+    for i in range(len(ids)):
+        plot_values(get_subreddit_from_id(ids[i][0]), ids[i], df_averages[i], 1, 1, 1)
+        bar.next()
+    bar.finish()
+    plt.show()
